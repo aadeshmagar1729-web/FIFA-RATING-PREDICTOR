5a23bcdea18e514a9f98c8cd37bff2cdf4f79f69
Aadesh <magaraadesh1208@gmail.com>
2026-07-03 21:00:34 +0530

    Add Vercel frontend URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://fifa-rating-predictor.vercel.app",
 ]
 
 ROOT_URLCONF = 'backend.urls'
